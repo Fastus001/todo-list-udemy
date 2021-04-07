db9ca1d0579d629d84cc2d6ab0fafc2829279b66
Tomek <tkarczmarzyk@gmail.com>
2021-04-07 12:26:25 +0200

    Angular kurs - ngFor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Task} from './task';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-list';
+  config: { [key: string]: string | Date} = null;
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false,
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2020-01-03',
+      done: false,
+    },
+    {
+      name: 'Sprzątanie kuwety',
+      deadline: '2020-01-04',
+      done: false,
+    },
+  ];
+
+
+  constructor() {
+    setTimeout(() => {
+      this.config = {
+        title: 'Lista Zadań',
+        footer: '© Lista Zadań zbudowana w angularze.',
+        date: new Date()
+      };
+    }, 500);
+
+
+  }
 }
